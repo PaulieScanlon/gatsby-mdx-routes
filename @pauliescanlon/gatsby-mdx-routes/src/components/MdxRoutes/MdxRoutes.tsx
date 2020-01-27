2e27827bfd88fc041fe708f2d74c8c3f8ba2c749
pauliescanlon <pauliescanlon@gmail.com>
2020-01-27 10:01:11 +0000

    add useStaicQuery remove staticQuery

--- @pauliescanlon/gatsby-mdx-routes/src/components/MdxRoutes/MdxRoutes.tsx
+++ @pauliescanlon/gatsby-mdx-routes/src/components/MdxRoutes/MdxRoutes.tsx
@@ -1,5 +1,5 @@
-import React, { FunctionComponent } from "react"
-import { StaticQuery, graphql } from "gatsby"
+import React, { FunctionComponent, Fragment } from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import { recursiveMenu } from "../../utils/recursiveMenu"
 
@@ -10,58 +10,61 @@ interface IMdxRoutesProps {
   children: any
 }
 
+interface IMdxData {
+  /** navigationLabel from frontmatter */
+  navigationLabel: string
+  /** slug from graphql query */
+  slug: string
+}
+
 export const MdxRoutes: FunctionComponent<IMdxRoutesProps> = ({
   children,
   navigationOrder,
 }) => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query {
-          allMdx {
-            edges {
-              node {
-                id
-                fields {
-                  slug
-                }
-                frontmatter {
-                  title
-                  navigationLabel
-                }
-              }
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              navigationLabel
             }
           }
         }
-      `}
-      render={data => {
-        const { edges } = data.allMdx
+      }
+    }
+  `)
+  const { edges } = data.allMdx
 
-        const sortOrder = (array: any) => {
-          return array.sort((a: any, b: any) => {
-            if (navigationOrder) {
-              return (
-                navigationOrder.indexOf(a.navigationLabel) -
-                navigationOrder.indexOf(b.navigationLabel)
-              )
-            }
-            return a.navigationLabel - b.navigationLabel
-          })
-        }
+  const sortOrder = (array: IMdxData[]) => {
+    return array.sort((a: any, b: any) => {
+      if (navigationOrder) {
+        return (
+          navigationOrder.indexOf(a.navigationLabel) -
+          navigationOrder.indexOf(b.navigationLabel)
+        )
+      }
+      return a.navigationLabel - b.navigationLabel
+    })
+  }
 
-        const mdxData = edges.map((data: any) => {
-          const { frontmatter, fields } = data.node
-          return {
-            navigationLabel: frontmatter.navigationLabel,
-            slug: fields.slug,
-          }
-        })
-
-        const routes = sortOrder(mdxData)
-        const menus = sortOrder(recursiveMenu(mdxData))
-
-        return children(routes, menus)
-      }}
-    />
+  const mdxData = edges.map(
+    (data: any): IMdxData => {
+      const { frontmatter, fields } = data.node
+      return {
+        navigationLabel: frontmatter.navigationLabel,
+        slug: fields.slug,
+      }
+    }
   )
+
+  const routes = sortOrder(mdxData)
+  const menus = sortOrder(recursiveMenu(mdxData))
+
+  return <Fragment>{children(routes, menus)}</Fragment>
 }
